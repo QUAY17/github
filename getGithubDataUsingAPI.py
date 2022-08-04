79ddcb00585dfca53229f2d446368eebce4b4f01
Jennifer Quay Minnich <jminniecc@gmail.com>
2022-08-04 16:46:20 -0600

    Create getGithubDataUsingAPI.py

---
+++ getGithubDataUsingAPI.py
@@ -0,0 +1,82 @@
+import json
+import requests
+
+import pprint as pp
+
+gh_user="QUAY17"
+gh_token="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"
+gh_repo="tensorflow/tensorflow"
+
+# Get all Commits since repo creation
+gitHubAPI_URL_getCommits = f"https://api.github.com/repos/{gh_repo}/commits?branch=master&page=1&per_page=100"
+response = requests.get(gitHubAPI_URL_getCommits, auth=(gh_user, gh_token))
+data = response.json()
+while 'next' in response.links.keys():
+  response=requests.get(response.links['next']['url'], auth=(gh_user, gh_token))
+  data.extend(response.json())
+print("MetaData:")
+print("---")
+print(response)
+print(type(data))
+print("<records [{}]>".format(len(data)))
+print("---")
+#print("Record[0]:")
+#pp.pprint(data[0])
+with open('gh_Commits.json', 'w') as jsonFile:
+    json.dump(data, jsonFile, indent=4)
+
+
+# Get all Comments since repo creation
+gitHubAPI_URL_getComments = f"https://api.github.com/repos/{gh_repo}/comments?page=1&per_page=100"
+response = requests.get(gitHubAPI_URL_getComments, auth=(gh_user, gh_token))
+data = response.json()
+while 'next' in response.links.keys():
+  response=requests.get(response.links['next']['url'], auth=(gh_user, gh_token))
+  data.extend(response.json())
+print("MetaData:")
+print("---")
+print(response)
+print(type(data))
+print("<records [{}]>".format(len(data)))
+print("---")
+#print("Record[0]:")
+#pp.pprint(data[0])
+with open('gh_Comments.json', 'w') as jsonFile:
+    json.dump(data, jsonFile, indent=4)
+
+# Get all Pull Requests since repo creation
+gitHubAPI_URL_getPulls = f"https://api.github.com/repos/{gh_repo}/pulls?branch=master&state=all&page=1&per_page=100"
+response = requests.get(gitHubAPI_URL_getPulls, auth=(gh_user, gh_token))
+data = response.json()
+while 'next' in response.links.keys():
+  response=requests.get(response.links['next']['url'], auth=(gh_user, gh_token))
+  data.extend(response.json())
+print("MetaData:")
+print("---")
+print(response)
+print(type(data))
+print("<records [{}]>".format(len(data)))
+print("---")
+#print("Record[0]:")
+#pp.pprint(data[0])
+with open('data/gh_Pulls.json', 'w') as jsonFile:
+    json.dump(data, jsonFile, indent=4)
+
+# Get all Issues since repo creation
+gitHubAPI_URL_getIssues = f"https://api.github.com/repos/{gh_repo}/issues?&state=all&page=1&per_page=100"
+response = requests.get(gitHubAPI_URL_getIssues, auth=(gh_user, gh_token))
+data = response.json()
+while 'next' in response.links.keys():
+  response=requests.get(response.links['next']['url'], auth=(gh_user, gh_token))
+  data.extend(response.json())
+print("MetaData:")
+print("---")
+print(response)
+print(type(data))
+print("<records [{}]>".format(len(data)))
+print("---")
+#print("Record[0]:")
+#pp.pprint(data[0])
+with open('data/gh_Issues.json', 'w') as jsonFile:
+    json.dump(data, jsonFile, indent=4)
+
