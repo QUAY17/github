8bbe33271fad4e48eba68fc1f40c24dac10f1ee4
Jennifer Minnich <jminnich@icasa.nmt.edu>
2022-09-15 11:34:48 -0600

    adding issue creation / pull creation in correct format. also fixed generatd id problem - we need to keep some of the ids for multiple events .

--- githubToFormattedJson.py
+++ githubToFormattedJson.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import random
 import string
 import hashlib
+import bcrypt
 
 def usage():
     print("python githubToJson.py gh_Issues.json gh_Pulls.json finalOutFile.json")
@@ -15,56 +16,91 @@ def usage():
     print("\tfinalOutFile.json - a json file that will contain all the Github data we find useful")
     exit(0)
 
-# generation of relational ID
-def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+# generates relational ID
+def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-# anonymizing entity ID
+# anonymizes entity ID
 def salt_hash_id(issueUserId):
-    githubId = issueUserId
-    salt = os.urandom(32)
-    new_id = githubId+salt
-    hashed = hashlib.md5(new_id.encode())
-    return hashed
+    githubId = str(issueUserId).encode()
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(githubId, salt)
+    return hash[:16]
 
 if __name__ == "__main__":
     if len(argv) != 4:
         usage()
 
-    with open(argv[1], "rt") as issuesJson:
+    with open(argv[1], "rt") as issuesJson: # Issues
         issues = json.load(issuesJson)
-   
-    dataData = []
 
-    for attribute in issues[:100]:
-        issueCreatedAt = attribute["created_at"]
-        issueUserId = attribute["user"]["id"]
-        anonUserId = salt_hash_id(issueUserId)
-        eventUrl = attribute["events_url"]
-        issueId = id_generator()
-        message= attribute["body"]
-
-        gh_user="QUAY17"
-        gh_token="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"
-
-        gitHubAPI_URL_getEvents = f"{eventUrl}"
-        response = requests.get(gitHubAPI_URL_getEvents, auth=(gh_user, gh_token))
-        issueComment = response.json()
-
-
+    data = []
     
-        dataDict = {"Timestamp":issueCreatedAt, "EntityId":anonUserId, "Symbol":eventName, "Relational IDs":issueId, "Context":message}
+    for attribute in issues[:10]:
+    # Issue Creation
+        relationalalIds = []
+        if attribute["created_at"]:
+            issueNumber = attribute["number"] # To match issue and pr
+            issueCreatedAt = attribute["created_at"] # Timestamp
+            issueUserId = attribute["user"]["id"] # Entity Ids []
+            #issueUserId = salt_hash_id(issueUserId)
+            entityIds = []
+            entityIds.append(issueUserId)
+            issueMessage = attribute["body"] # Context
+            issueAssignees = attribute["assignees"]
+            for attribute in issueAssignees:
+                issueAssigneesId = attribute["id"]
+                #issueAssigneesId = salt_hash_id(issueAssigneesId)
+                entityIds.append(issueAssigneesId)
+            eventName = "Issue Creation" # Symbol Name
+            issueId = id_generator() # Issue Id
+            sameIssueId = issueId # when we need id to be the same for multiple events
+            relationalalIds.append(issueId)
 
+            issueDict = {"Issue Number":issueNumber,"Timestamp":issueCreatedAt, "EntityIds":entityIds, "Symbol":eventName, "Relational ID":relationalalIds, "Context":issueMessage}
+
+            data.append(issueDict)
+
+            with open(argv[2], "rt") as pullsJson: # Pulls
+                pulls = json.load(pullsJson)
+
+            for attribute in pulls:        
+            # PR Creation
+                pullNumber = attribute["number"]
+                if pullNumber == issueNumber:
+                    pullCreatedAt = attribute["created_at"]
+                    pullUserId = attribute["user"]["id"]
+                    entityIds = []
+                    entityIds.append(pullUserId)
+                    pullAssignees = attribute["assignees"]
+                    pullReviewers = attribute["requested_reviewers"] 
+                    for attribute in pullAssignees:
+                        pullAssigneesId = attribute["id"]
+                        entityIds.append(pullAssigneesId)
+                    for attribute in pullReviewers:
+                        pullReviewersId = attribute["id"]
+                        entityIds.append(pullReviewersId)
+                    eventName = "Pull Request Creation" # Symbol Name
+                    prId = id_generator() # Pull Request Id
+                    samePrId = prId # when we need id to be the same for multiple events
+                    relationalalIds = [sameIssueId, prId]
+
+                    prDict = {"Pull Number": pullNumber,"Timestamp":pullCreatedAt, "EntityIds":entityIds, "Symbol":eventName, "Relational IDs":relationalalIds, "Context":issueMessage}
+
+                    data.append(prDict)
 
     # Header info ___________________________________________________________________________________
 
     dataName = "Github Data for Tensorflow"
     dataDate = "2015-11-07T01:19:20Z"
-    dataStart = issueCreatedAt[0]
-    dataEnd = issueCreatedAt[-1]
+    dataStart = issues[-1]["created_at"]
+    dataEnd = issues[0]["created_at"]
     dataVersion = 1.0
     dataOrigin = "Utlizes data/gh_Issues.json, gh_Pulls.json and dynamic requests to the Github API"
 
-        
-    githubDataFormatted = {"Data Name":dataName, "Creation Date":dataDate, "Data Range Start":dataStart, "Data Range End":dataEnd,
-            "Version Information":dataVersion, "Provenance Information":dataOrigin, "data":dataData}
+    # Header info ___________________________________________________________________________________
+    
+    githubDataFormatted = {"Data Name":dataName, "Creation Date":dataDate, "Data Range Start":dataStart, "Data Range End":dataEnd, "Version Information":dataVersion, "Provenance Information":dataOrigin, "data":data}
+    
+    with open(argv[3], "wt") as outFile:
+        json.dump(githubDataFormatted, outFile, indent=4)
